cd20ba6291037d7a3195b271f26b66afb5f5d60c
Samuel Stauffer <samuel@descolada.com>
2009-10-21 20:19:41 -0700

    Added setup.py

--- erlastic/__init__.py
+++ erlastic/__init__.py
@@ -1,6 +1,8 @@
 
 """Erlang External Term Format serializer/deserializer"""
 
+__version__ = "0.0.1"
+
 from erlastic.codec import ErlangTermDecoder, ErlangTermEncoder, Atom, Binary
 
 def encode(obj):
---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from erlastic import __version__ as version
+
+setup(
+    name = 'erlastic',
+    version = version,
+    description = 'Erlastic',
+    author = 'Samuel Stauffer',
+    author_email = 'samuel@lefora.com',
+    url = 'http://github.com/samuel/python-erlastic',
+    packages = ['erlastic'],
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
